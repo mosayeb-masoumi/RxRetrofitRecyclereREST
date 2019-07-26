a4f5a5d8c02778509722f3e23aaa070fa768b0a2
mosayeb_masoumi <mosayeb.masoumi.co@gmail.com>
2019-07-26 18:34:40 +0430

    add exception while request faled

--- app/src/main/java/com/example/rxretrofitrecyclererest/MainActivity.java
+++ app/src/main/java/com/example/rxretrofitrecyclererest/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.widget.Toast;
 
 import com.example.rxretrofitrecyclererest.adapter.PostAdapter;
 import com.example.rxretrofitrecyclererest.retrofit.APIService;
@@ -11,8 +12,10 @@ import com.example.rxretrofitrecyclererest.retrofit.APIClient;
 
 import java.util.List;
 
+import io.reactivex.Observer;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.disposables.Disposable;
 import io.reactivex.functions.Consumer;
 import io.reactivex.schedulers.Schedulers;
 import retrofit2.Retrofit;
@@ -35,7 +38,7 @@ public class MainActivity extends AppCompatActivity {
         myAPI = retrofit.create(APIService.class);
 
         //view
-        recycler_post= findViewById(R.id.recycler_post);
+        recycler_post = findViewById(R.id.recycler_post);
         recycler_post.setHasFixedSize(true);
         recycler_post.setLayoutManager(new LinearLayoutManager(this));
 
@@ -43,31 +46,36 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    private void fetchData() {
+   private void fetchData() {
         compositeDisposable.add(myAPI.getPosts()
         .subscribeOn(Schedulers.io())
         .observeOn(AndroidSchedulers.mainThread())
         .subscribe(new Consumer<List<Post>>() {
-            @Override
-            public void accept(List<Post> posts) throws Exception {
-                displayData(posts);
+                       @Override
+                       public void accept(List<Post> posts) throws Exception {
+                           displayData(posts);
 
 //                PostAdapter adapter = new PostAdapter(MainActivity.this,posts);
 //                recycler_post.setAdapter(adapter);
-            }
-        }));
+                       }
+                   }, new Consumer<Throwable>() {
+                       @Override
+                       public void accept(Throwable t) throws Exception {
+                           Toast.makeText(MainActivity.this, ""+t.getMessage(), Toast.LENGTH_SHORT).show();
+                       }
+                   }
+
+        ));
 
     }
+
 
 
     private void displayData(List<Post> posts) {
 
-        PostAdapter adapter = new PostAdapter(this,posts);
+        PostAdapter adapter = new PostAdapter(this, posts);
         recycler_post.setAdapter(adapter);
     }
-
-
-
 
 
     @Override
